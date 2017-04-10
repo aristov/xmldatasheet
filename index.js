527b71142495be3a45e32c8a988f4d705ebfffdf
aristov <vv.aristov@gmail.com>
2017-04-11 02:00:42 +0300

    add sources

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-import './test/htmlfetch'
+import './test/xmlfetch'
--- test/xmlfetch.js
+++ test/xmlfetch.js
@@ -2,7 +2,12 @@ import { domTree } from '../lib/domtree'
 
 const parser = new DOMParser
 
-fetch('test/data/data.xml')
+const URI1 = 'test/data/22-rdf-syntax-ns.rdf'
+const URI2 = 'test/data/rdf-schema.rdf'
+const URI3 = 'test/data/owl.rdf'
+const URI4 = 'test/data/studio.rdf.xml'
+
+fetch(URI4)
     .then(res => res.text())
     .then(xml => parser.parseFromString(xml, 'text/xml'))
     .then(doc => domTree({
