8c0cd81c84bd438231a99ad6c6b9266f3f8004cc
aristov <vv.aristov@gmail.com>
2017-04-12 02:08:28 +0300

    styling

--- test/data/studio.rdf.js
+++ test/data/studio.rdf.js
@@ -2,32 +2,31 @@ const { forEach, reduce } = Array.prototype
 const labels = document.querySelectorAll('label')
 const subClassOfs = document.querySelectorAll('subClassOf')
 const RE = /^.*#/
-const xmlns = reduce.call(
-    document.documentElement.attributes,
-    (res, attr) => {
-        res[attr.value] = attr.localName
-        return res
-    }, {})
+const attributes = document.documentElement.attributes
+const xmlns = reduce.call(attributes, (res, { localName, value }) => {
+    res[value] = localName
+    return res
+}, {})
 forEach.call(labels, node => {
     if(node.hasAttribute('xml:lang')) {
         node.setAttribute('aria-hidden', 'true')
     }
     else {
-        const about = node.parentNode.getAttribute('rdf:about')
+        const uri = node.parentNode.getAttribute('rdf:about')
         const link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
-        const parent = node.parentNode
-        const pair = about.split('#')
-        link.href = about
+        const pair = uri.split('#')
+        link.href = uri
         link.textContent = xmlns[pair[0] + '#'] + ':' + pair[1]
-        parent.insertBefore(link, node.nextSibling)
-        parent.insertBefore(new Text(' '), node.nextSibling)
+        const { parentNode, nextSibling } = node
+        parentNode.insertBefore(new Text(' '), nextSibling)
+        parentNode.insertBefore(link, nextSibling)
     }
 })
 forEach.call(subClassOfs, node => {
     const uri = node.getAttribute('rdf:resource')
     const link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a')
     const pair = uri.split('#')
-    link.setAttribute('href', uri)
+    link.href = uri
     link.textContent = xmlns[pair[0] + '#'] + ':' + pair[1]
     node.appendChild(new Text(node.nodeName + ': '))
     node.appendChild(link)
